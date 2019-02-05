0ee2eeede3be0a872018a8d977e978ff17dcb6c7
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2019-02-05 12:30:08 -0300

    Se crearon funcionesw que permiten obtener un listado de todas las zonas horarias.

--- TimeZoneService/Function.cs
+++ TimeZoneService/Function.cs
@@ -27,18 +27,48 @@ namespace TimeZoneService
         /// </summary>
         /// <param name="request"></param>
         /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        public APIGatewayProxyResponse GetAllTimeZones(APIGatewayProxyRequest request, ILambdaContext context)
         {
             context.Logger.LogLine("Get Request\n");
+            List<TimeZoneInfo> timeZones = new List<TimeZoneInfo>();
+
+            foreach(var location in TzdbDateTimeZoneSource.Default.ZoneLocations)
+            {
+                timeZones.Add(GetZoneInfo(location));
+            }
 
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
-                Body = "Hello AWS Serverless",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                Body = JsonConvert.SerializeObject(timeZones),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
 
             return response;
+        }
+
+        private TimeZoneInfo GetZoneInfo(TzdbZoneLocation location)
+        {
+            var zone = DateTimeZoneProviders.Tzdb[location.ZoneId];
+
+            // Get the start and end of the year in this zone
+            var startOfYear = zone.AtStartOfDay(new LocalDate(2017, 1, 1));
+            var endOfYear = zone.AtStrictly(new LocalDate(2018, 1, 1).AtMidnight().PlusNanoseconds(-1));
+
+            // Get all intervals for current year
+            var intervals = zone.GetZoneIntervals(startOfYear.ToInstant(), endOfYear.ToInstant()).ToList();
+
+            // Try grab interval with DST. If none present, grab first one we can find
+            var interval = intervals.FirstOrDefault(i => i.Savings.Seconds > 0) ?? intervals.FirstOrDefault();
+
+            return new TimeZoneInfo
+            {
+                TimeZoneId = location.ZoneId,
+                Offset = interval.StandardOffset.ToTimeSpan(),
+                DstOffset = interval.WallOffset.ToTimeSpan(),
+                CountryCode = location.CountryCode,
+                CountryName = location.CountryName
+            };
         }
     }
 }
